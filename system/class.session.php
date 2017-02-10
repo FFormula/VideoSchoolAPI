94723637b565ac2c0cf5f350d875270c17c74104
VEV Server <formulist@gmail.com>
2017-02-10 23:31:54 +0300

    session class

---
+++ system/class.session.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace system;
+
+class session
+{
+    public static function get_user ()
+    {
+        if (isset ($_SESSION ["user"]))
+            return $_SESSION ["user"];
+        return array ();
+    }
+    
+    public static function set_user ($user)
+    {
+        $_SESSION ["user"] = $user;
+    }
+    
+    public static function is_logged ()
+    {
+        if (isset ($_SESSION ["user"] ["id"]))
+            return true;
+        return false;
+    }
+
+}
