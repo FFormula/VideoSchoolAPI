edbbfa0b8ca618af85625e08b2f03e8e2813079b
Jevgenij Volosatov <fformula@gmail.com>
2017-02-23 14:54:41 +0200

    Move lang texts into database

--- page/class.info.php
+++ page/class.info.php
@@ -6,10 +6,11 @@ class info extends \system\resultable
 {
     public function index ($get)
     {
+        global $lang;
         include_lang ("menu");
         include_lang ("info");
 
-        $this->array["title"] = "База Водителей";
+        $this->array["title"] = $lang["info.bad drivers database"];
         $this->array["menu"] = "info/index";
 
         return true;
